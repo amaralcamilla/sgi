6ec40cc8cfaca76c844d903f40cb5cf4c68cfdca
Camilla <camillaamaral7@gmail.com>
2021-11-19 19:23:58 -0300

    Importa Swal.

--- src/components/ProductForm/index.js
+++ src/components/ProductForm/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
 const ProductForm = () => {
   const [imageUrl, setImageUrl] = useState("");
