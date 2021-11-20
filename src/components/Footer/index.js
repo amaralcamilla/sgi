6245b91a9c0bd0fbe507eaf94260a3e8ca79568c
Camilla <camillaamaral7@gmail.com>
2021-11-20 10:15:35 -0300

    Corrige tag link para nova aba.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,22 +10,22 @@ const Footer = () => {
     <footer>
       <ul className="footer-list">
         <li className="footer-item">
-          <a href="https://www.facebook.com/" target="_blank">
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
             <IoLogoFacebook className="icon" size="20" />
           </a>
         </li>
         <li className="footer-item">
-          <a href="https://www.linkedin.com/in/amaralcamilla/" target="_blank">
+          <a href="https://www.linkedin.com/in/amaralcamilla/" target="_blank" rel="noopener noreferrer">
             <IoLogoLinkedin className="icon" size="20" />
           </a>
         </li>
         <li className="footer-item">
-          <a href="https://www.instagram.com/" target="_blank">
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
             <IoLogoInstagram className="icon" size="20" />
           </a>
         </li>
         <li className="footer-item">
-          <a href="https://www.youtube.com/" target="_blank">
+          <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
             <IoLogoYoutube className="icon" size="20" />
           </a>
         </li>
